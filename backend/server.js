302b8ca3c54a1e08fdcae619025d0a4229e13938
unknown <you@example.com>
2023-08-25 09:52:58 +0530

    database function changes

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,16 @@ const {
   readTeacherInfo,
   updateStudent,
   updateTeacher,
+  dbinitialize,
 } = require ("./database.js");
 
 const app = express();
 const bodyParser = require  ("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Initialize the database
+dbinitialize();
 
 app.get("/listTeachers", async function (req, res) {
   console.log("Request received to list teachers");
